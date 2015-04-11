6fd8838914862c7f7507a05bb7e5c3ec7c649afe
Pablo Faust <pabfaust@gmail.com>
2015-04-11 20:07:54 +0900

    Mobile 1

--- home/body.php
+++ home/body.php
@@ -1,21 +1,27 @@
 <div class="container-fluid body">
 
-	 <div class="blue-section">
-	 	<h1>Drapeaux, pavillons, mâts<br>Articles pour mairies et collectivités</h1>
-	 </div>
+	<div class="wrapper-1">
+		 <div class="blue-section">
+		 	<h1>Drapeaux, pavillons, mâts<br>Articles pour mairies et collectivités</h1>
+		 </div>
+	</div>
 
-	 <div class="white-section">
-	 	<h2>Contact</h2>
-	 	<h3><strong>COUTARD PAVOISEMENT</strong><br>
-	 		ROUTE DE SAINT-LO<br>
-	 		14400 BAYEUX<br>
-	 		<a href="mailto:coutard.pavoisement@hotmail.fr">coutard.pavoisement@hotmail.fr</a><br>
-	 		TEL : 2 31 92 85 24<br>
-	 		FAX : 2 31 92 99 22</h3>
-	 </div>
+	<div class="wrapper-2">
+		 <div class="white-section">
+		 	<h2>Contact</h2>
+		 	<h3><strong>COUTARD PAVOISEMENT</strong><br>
+		 		ROUTE DE SAINT-LO<br>
+		 		14400 BAYEUX<br>
+		 		<a href="mailto:coutard.pavoisement@hotmail.fr">coutard.pavoisement@hotmail.fr</a><br>
+		 		TEL : 2 31 92 85 24<br>
+		 		FAX : 2 31 92 99 22</h3>
+		 </div>
+	</div>
 
-	 <div class="red-section">
-	 	 <h4>Demandez un devis</h4>
-	 	 <h5>Nous vous répondrons au plus vite !</h5>
-	 </div>
+	<div class="wrapper-3">
+		 <div class="red-section">
+		 	 <h4>Demandez un devis</h4>
+		 	 <h5>Nous vous répondrons au plus vite !</h5>
+		 </div>
+	</div>
 </div>
