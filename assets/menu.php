c7d9c882cfa4033c4e5a5bff8580764edda2e27d
Pablo Faust <pabfaust@gmail.com>
2015-04-15 15:47:59 +0900

    Début tablette horizontale

--- assets/menu.php
+++ assets/menu.php
@@ -1,11 +1,13 @@
 <div class="container-fluid menu">
 	<div class="row">
 
-		<div class="col-xs-12 col-sm-6 column-1">
+		<!-- Le logo s'affiche au centre ou à gauche selon mobile ou tablette verticale -->
+		<div class="col-xs-12 col-sm-6 column-1 hidden-md">
 			<img src="assets/img/Logo.png" />
 		</div>
 
-		<div class="col-sm-6 column-2 hidden-xs">
+		<!-- Apparition d'un toggle en version tablette verticale -->
+		<div class="col-sm-6 column-2 hidden-xs hidden-md">
 			<div class="dropdown">
 			  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
 			    <i class="fa fa-bars fa-3x"></i>	
@@ -17,7 +19,34 @@
 			    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Demande de devis</a></li>
 			  </ul>
 			</div>
-
 		</div>
+
+		<div class="col-md-4 column-1">
+			<ul class="menu-md">
+				<div class="row">
+					<div class="col-md-6 lien-1">
+						<li><h2>Communication et signalétique</h2></li>
+					</div>
+					<div class="col-md-6 lien-2">
+						<li><h2>Articles de mairie</h2></li>
+					</div>
+				</div>
+			</ul>
+		</div>
+		<div class="col-md-4 column-2">
+			<img src="assets/img/Logo.png" />
+		</div>
+		<div class="col-md-4 column-3">
+			<ul class="menu-md">
+				<div class="row">
+					<div class="col-md-6 lien-3">
+						<li><h2>Drapeaux, mâts et supports</h2></li>
+					</div>
+					<div class="col-md-6 lien-4">
+						<li><h2>Demande de devis</h2></li>
+					</div>
+			</ul>
+		</div>
+
 	</div>
 </div>
