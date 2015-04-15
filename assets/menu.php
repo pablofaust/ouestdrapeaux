bd92015f0488646911cbeffd41c93fd05cad7397
Pablo Faust <pabfaust@gmail.com>
2015-04-15 10:17:30 +0900

    Tableau menu en bonne voie

--- assets/menu.php
+++ assets/menu.php
@@ -1,20 +1,23 @@
 <div class="container-fluid menu">
 	<div class="row">
+
 		<div class="col-xs-12 col-sm-6 column-1">
 			<img src="assets/img/Logo.png" />
 		</div>
+
 		<div class="col-sm-6 column-2 hidden-xs">
 			<div class="dropdown">
-				  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-				    <i class="fa fa-bars fa-3x"></i>
-				  </button>
-				  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-				    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-				    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-				    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-				    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
-				  </ul>
+			  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+			    <i class="fa fa-bars fa-3x"></i>	
+			  </button>
+			  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+			    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Communication et Signalétique</a></li>
+			    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Articles de mairie</a></li>
+			    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Drapeaux, mâts et supports</a></li><br>
+			    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Demande de devis</a></li>
+			  </ul>
 			</div>
+
 		</div>
 	</div>
 </div>
--- home/index.php
+++ home/index.php
@@ -4,7 +4,10 @@
 <body>
 <?php include $route.'/assets/menu.php' ?>
 
-<?php include $route.'/home/body.php' ?>
+<?php include $route.'/home/body.php' ?> 
 <?php include $route.'/home/form.php' ?>
 
 </body>
+
+
+<?php include $route.'/assets/footer.php' ?>
