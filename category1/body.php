2e5ee1dd2aea005509aaecd1637c92d65b2c9762
Pablo Faust <pabfaust@gmail.com>
2015-05-17 09:59:22 -0500

    Catégories iPad

--- category1/body.php
+++ category1/body.php
@@ -5,19 +5,41 @@
 		<div class="col-sm-10 col-sm-offset-1 col-md-7 column-1">
 			<h2>Communication & Signalétique</h2><br>
 
-			<h3>Vous trouverez ici tous les articles dont vous pouvez avoir besoin pour l’organisation
-			de vos <strong>événements officiels</strong> : articles liés au protocole, aux inaugurations, etc.
-			Nous vous proposons également un large choix de <strong>médailles et leurs accessoires</strong>.
-			Enfin, nous mettons à voitre disposition toute une gamme d’équipements dont vous pouvez
-			avoir besoin pour vos <strong>aménagements intérieurs et extérieurs</strong> (panneaux de rue, miroirs
-			de voirie, poteaux à sangle, vitrines, grilles d’exposition, etc.).<br><br>
+			<h3>Réalisé en matière souple, aisément fixé et facilement transportable, ce <strong>support de communication
+				événementielle</strong> est très prisé pour habiller de grandes longueurs le long de circuits, pistes,
+				stades, places, etc. <strong>Le choix des dimensions est quasi-libre</strong> et <strong>le montage est adapté</strong>	 à la structure
+				sur laquelle il est fixé : barrière, clôture, cadre métallique, directement sur une façade ou tout autre.<br><br>
 
-			N'hésitez pas à nous contacter pour que nous vous aidions à sélectionner les produits dont vous avez besoin.
+			<strong>Du sur-mesure personnalisé, d'un exemplaire à plusieurs milliers !</strong> Prix sur mesure : nous contacter.
 			</h3>
 
 		</div> 
 
-	</div>
+	<!-- tablette horizontale -->
+
+		<div class="links-md-vertical">
+
+			<div class="col-md-3">
+				<button type="button" class="btn">
+				<h4>Structures</h4>
+				</button>
+			</div>
+
+			<div class="col-md-3">
+				<button type="button" class="btn">
+				<h4>Porte-bannières</h4>
+				</button>
+			</div>
+
+			<div class="col-md-3">
+				<button type="button" class="btn">
+				<h4>Murs d'images</h4>
+				</button>
+			</div>
+
+		</div>
+
+	<!-- tablette verticale -->
 
 	<div class="row links-sm-vertical hidden-md">
 
