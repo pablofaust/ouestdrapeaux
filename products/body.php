3b400835dd382a6b1cb0421e1b9230d236103598
Pablo Faust <pabfaust@gmail.com>
2015-05-18 17:07:08 -0500

    Produits

---
+++ products/body.php
@@ -0,0 +1,90 @@
+<div class="container-fluid products">
+
+	<div class="product-item">
+
+			<div class="product-img">
+
+				<img src="<?php echo $url ?>home/img/produit1.jpg" />
+
+			</div>
+
+			<div class="product-description">
+
+				<h2>Lorem Ipsum</h2>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Nam sed porta orci. Curabitur luctus dui dui, ut vestibulum
+				dolor tristique tempus. Cras molestie consectetur ipsum,
+				eu aliquet ipsum auctor ut.</p>
+
+				<div class="contact-input">
+
+				<button type="button" class="btn">
+					<h4>Demander un devis</h4>
+				</button>
+
+				</div>
+
+			</div>	
+
+	</div>
+
+	<div class="product-item">
+
+			<div class="product-img">
+
+				<img src="<?php echo $url ?>home/img/produit2.jpg" />
+
+			</div>
+
+			<div class="product-description">
+
+				<h2>Lorem Ipsum</h2>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Nam sed porta orci. Curabitur luctus dui dui, ut vestibulum
+				dolor tristique tempus. Cras molestie consectetur ipsum,
+				eu aliquet ipsum auctor ut.</p>
+
+				<div class="contact-input">
+
+				<button type="button" class="btn">
+					<h4>Demander un devis</h4>
+				</button>
+
+				</div>
+
+			</div>	
+
+	</div>
+
+	<div class="product-item">
+
+			<div class="product-img">
+
+				<img src="<?php echo $url ?>home/img/produit3.jpg" />
+
+			</div>
+
+			<div class="product-description">
+
+				<h2>Lorem Ipsum</h2>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Nam sed porta orci. Curabitur luctus dui dui, ut vestibulum
+				dolor tristique tempus. Cras molestie consectetur ipsum,
+				eu aliquet ipsum auctor ut.</p>
+
+				<div class="contact-input">
+
+				<button type="button" class="btn">
+					<h4>Demander un devis</h4>
+				</button>
+
+				</div>
+
+			</div>	
+
+	</div>
+
+</div>
